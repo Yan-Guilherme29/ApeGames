665894ec3ee7c977c34b2253cf7cce190315a4d8
Yan Guilherme Siqueira Gomides <yanguilherme2927@gmail.com>
2026-02-17 19:47:12 -0300

    Organização e criação da classe Personagem

---
+++ Models/Personagem.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace SimiosGame.Models
+{
+    public class Personagem
+    {
+        public string Nome { get;set;}
+        public int Vida { get;set;}
+        public int Forca {get;set;}
+    
+        public Personagem (string nome, int vida, int forca) {
+            this.Nome = nome;
+            this.Vida = vida;
+            this.Forca = forca;
+        }
+
+
+        public void Atacar(Personagem alvo )
+        {
+            Console.WriteLine($"{Nome} atacou {alvo.Nome} com o total de {Forca} danos de força!");
+            alvo.ReceberDano(Forca);
+        }
+
+
+        public void ReceberDano(int dano)
+        {
+            Vida -= dano;
+                if (Vida < 0)
+            {
+                Vida = 0;
+            }
+
+            Console.WriteLine($"{Nome} recebeu {dano} de dano! \n Vida restante: {Vida}");
+
+        }
+    }
+}
+
+
+
